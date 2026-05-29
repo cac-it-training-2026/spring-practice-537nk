f8af855e7f194c783925e2b0e16675222eae55a9
knamba <knamba@C010423464.zipangu.cac.co.jp>
2026-05-29 17:28:54 +0900

    practice0502まで完了

--- spring_practice/src/main/java/jp/co/sss/practice/p04/q02/controller/Practice0402Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p04/q02/controller/Practice0402Controller.java
@@ -1,5 +1,43 @@
 package jp.co.sss.practice.p04.q02.controller;
 
-public class Practice0402Controller {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class Practice0402Controller {
+	@RequestMapping(path = "/numguess/start", method = RequestMethod.GET)
+	public String start(HttpSession session) {
+		Integer hitNumber = (int) (Math.floor(Math.random() * 9) + 1);
+		session.setAttribute("hitNumber", hitNumber);
+		System.out.println(hitNumber);
+		return "practice04/02/numguess_start";
+	}
+
+	@RequestMapping(path = "/numguess/input", method = RequestMethod.GET)
+	public String input() {
+		return "practice04/02/numguess_input";
+	}
+
+	@RequestMapping(path = "/numguess/judge", method = RequestMethod.GET)
+	public String judge(HttpSession session, Integer inputNum) {
+		Integer hitNumber = (Integer) session.getAttribute("hitNumber");
+		if (inputNum == hitNumber) {
+			return "redirect:/numguess/hit";
+		}
+		return "practice04/02/numguess_judge";
+	}
+
+	@RequestMapping(path = "/numguess/hit")
+	public String hit(Model model, HttpSession session) {
+		Integer hitNumber = (Integer) session.getAttribute("hitNumber");
+		String message = "当たりです！正解は" + hitNumber + "でした。";
+
+		session.removeAttribute("hitNumber");
+		model.addAttribute("message", message);
+		return "practice04/02/numguess_end";
+	}
 }
